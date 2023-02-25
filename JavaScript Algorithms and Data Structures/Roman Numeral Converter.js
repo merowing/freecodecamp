d2cf6ff5abcdffb292126a639506e16075d65342
elgynart <elgynart@gmail.com>
2023-02-25 19:37:00 +0200

    first method

--- JavaScript Algorithms and Data Structures/Roman Numeral Converter.js
+++ JavaScript Algorithms and Data Structures/Roman Numeral Converter.js
@@ -16,5 +16,45 @@ function convertToRoman(num) {
         1000: 'M',
     };
 
+    const keys = Object.keys(numbers);
+
+    function test(firstNumber, n = 0) {
+        let arr = [];
+        const rIndex = (num) => keys.findIndex(el => el > num) - 1;
+        let dividerIndex = rIndex(firstNumber);
+        let divider = (dividerIndex < 0) ? keys.at(-1) : keys[dividerIndex];
+        
+        while(firstNumber > 0) {
+            const remainder = firstNumber % divider;
+            
+            if(divider > 1 * 10**n) {
+                const num = firstNumber - remainder;
+                if(!keys.includes(num)) {
+                    let returnedArray = test(num, ++n);
+                    n -= 1;
+
+                    arr = [...arr, ...returnedArray];
+                }else {
+                    arr.push(num);
+                }
+
+                divider = keys[rIndex(remainder)];
+                firstNumber = remainder;
+            }else {
+                arr.push(divider);
+                firstNumber -= divider;
+            }
+        }
+
+        return arr;
+    }
+
+    const arabics = test(num);
+    num = (arabics.length)
+        ? arabics.map(item => numbers[item]).join('')
+        : 0;
+
     return num;
 }
+
+console.log(convertToRoman(45));
