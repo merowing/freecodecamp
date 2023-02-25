bf1bff33cfd8d3645fb4c5e20a1f9ce8c6d1e977
elgynart <elgynart@gmail.com>
2023-02-25 20:25:35 +0200

    changed some variables

--- JavaScript Algorithms and Data Structures/Roman Numeral Converter.js
+++ JavaScript Algorithms and Data Structures/Roman Numeral Converter.js
@@ -1,46 +1,50 @@
+const numbers = {
+    1: 'I',
+    4: 'IV',
+    5: 'V',
+    9: 'IX',
+    10: 'X',
+    40: 'XL',
+    50: 'L',
+    90: 'XC',
+    100: 'C',
+    400: 'CD',
+    500: 'D',
+    900: 'CM',
+    1000: 'M',
+};
+
 // method 1
 function convertToRoman(num) {
-    const numbers = {
-        1: 'I',
-        4: 'IV',
-        5: 'V',
-        9: 'IX',
-        10: 'X',
-        40: 'XL',
-        50: 'L',
-        90: 'XC',
-        100: 'C',
-        400: 'CD',
-        500: 'D',
-        900: 'CM',
-        1000: 'M',
-    };
 
     const keys = Object.keys(numbers);
 
-    function test(firstNumber, n = 0) {
+    function arabicsSequence(firstNumber, pow = 0) {
         let arr = [];
-        const rIndex = (num) => keys.findIndex(el => el > num) - 1;
-        let dividerIndex = rIndex(firstNumber);
-        let divider = (dividerIndex < 0) ? keys.at(-1) : keys[dividerIndex];
+        const previousIndex = (num) => {
+            return keys.findIndex(el => el > num) - 1;
+        }
+        const dividerIndex = previousIndex(firstNumber);
+        let divider = (dividerIndex < 0)
+            ? keys.at(-1)
+            : keys[dividerIndex];
         
-        while(firstNumber > 0) {
+        while (firstNumber > 0) {
             const remainder = firstNumber % divider;
             
-            if(divider > 1 * 10**n) {
+            if (divider > 1 * 10**pow) {
                 const num = firstNumber - remainder;
-                if(!keys.includes(num)) {
-                    let returnedArray = test(num, ++n);
-                    n -= 1;
-
+                if (!keys.includes(num)) {
+                    let returnedArray = arabicsSequence(num, ++pow);
                     arr = [...arr, ...returnedArray];
-                }else {
+                    pow -= 1;
+                } else {
                     arr.push(num);
                 }
 
-                divider = keys[rIndex(remainder)];
+                divider = keys[ previousIndex(remainder) ];
                 firstNumber = remainder;
-            }else {
+            } else {
                 arr.push(divider);
                 firstNumber -= divider;
             }
@@ -49,10 +53,10 @@ function convertToRoman(num) {
         return arr;
     }
 
-    const arabics = test(num);
+    const arabics = arabicsSequence(num);
     num = (arabics.length)
         ? arabics.map(item => numbers[item]).join('')
-        : 0;
+        : '';
 
     return num;
 }
