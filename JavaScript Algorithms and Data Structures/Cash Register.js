25a7981d56fb200376401c1805691c2408c9aeea
elgynart <elgynart@gmail.com>
2023-02-26 17:57:24 +0200

    method 2

--- JavaScript Algorithms and Data Structures/Cash Register.js
+++ JavaScript Algorithms and Data Structures/Cash Register.js
@@ -1,3 +1,79 @@
+// function checkCashRegister(price, cash, cid) {
+//     const statusString = ["OPEN", "CLOSED", "INSUFFICIENT_FUNDS"];
+//     const units = [
+//         ["PENNY", 0.01],
+//         ["NICKEL", 0.05],
+//         ["DIME", 0.1],
+//         ["QUARTER", 0.25],
+//         ["ONE", 1],
+//         ["FIVE", 5],
+//         ["TEN", 10],
+//         ["TWENTY", 20],
+//         ["ONE HUNDRED", 100]
+//     ];
+
+//     let difference = fixedValue(cash - price);
+//     let status = statusString[0];
+//     let moneyLeft = false;
+
+//     function fixedValue(val) {
+//         return parseFloat(val.toFixed(2));
+//     }
+
+//     let change = [...cid]
+//         .reverse()
+//         .reduce((arr, cid_unit) => {
+//             let [cid_currency, cid_value] = cid_unit;
+//             const [[, unit_value]] = units.filter(item => item[0] === cid_currency);
+            
+//             if (difference >= unit_value && cid_value > 0) {
+//                 let money = 0;
+//                 while (difference >= unit_value && cid_value > 0) {
+//                     difference = fixedValue(difference - unit_value);
+                    
+//                     money = fixedValue(money + unit_value);
+//                     cid_value = fixedValue(cid_value - unit_value);
+//                 }
+//                 arr.push([cid_currency, money]);
+//             }
+
+//             if (cid_value > 0) moneyLeft = true;
+
+//             return arr;
+//         }, []);
+
+//     if (difference > 0) {
+//         status = statusString[2];
+//         change = [];
+//     }
+//     if (difference === 0 && !moneyLeft) {
+//         status = statusString[1];
+//         change = [...cid];
+//     }
+
+//     return {
+//         status,
+//         change,
+//     };
+// }
+
+// console.log(checkCashRegister(
+//     3.26,
+//     100,
+//     [
+//         ["PENNY", 1.01],
+//         ["NICKEL", 2.05],
+//         ["DIME", 3.1],
+//         ["QUARTER", 4.25],
+//         ["ONE", 90],
+//         ["FIVE", 55],
+//         ["TEN", 20],
+//         ["TWENTY", 60],
+//         ["ONE HUNDRED", 100]
+//     ]
+// ));
+
+// method 2
 function checkCashRegister(price, cash, cid) {
     const statusString = ["OPEN", "CLOSED", "INSUFFICIENT_FUNDS"];
     const units = [
@@ -11,44 +87,50 @@ function checkCashRegister(price, cash, cid) {
         ["TWENTY", 20],
         ["ONE HUNDRED", 100]
     ];
-
-    let difference = fixedValue(cash - price);
-    let status = statusString[0];
-    let moneyLeft = false;
-
-    function fixedValue(val) {
-        return parseFloat(val.toFixed(2));
+    const fixedNumber = (val) => parseFloat(val.toFixed(2));
+    const getDefaultValue = (currency) => {
+        const index = units.findIndex(([item]) => item === currency);
+        return units[index][1];
     }
 
-    let change = [...cid]
-        .reverse()
-        .reduce((arr, cid_unit) => {
-            let [cid_currency, cid_value] = cid_unit;
-            const [[, unit_value]] = units.filter(item => item[0] === cid_currency);
+    let change = [];
+    let status = statusString[0];
+    let difference = fixedNumber(cash - price);
+    let drawerCash = cid.reduce((sum, [, val]) => {
+        return fixedNumber(sum + val)
+    }, 0);
+
+    cid.reverse();
+    for (let [currency, value] of cid) {
+        const defaultValue = getDefaultValue(currency);
+
+        if (difference >= defaultValue && value > 0) {
+            let remainder = fixedNumber((difference*100) % (defaultValue*100))/100;
+            const needCash = fixedNumber(difference - remainder);
+
+            const getCash = (needCash <= value)
+                ? needCash
+                : value;
             
-            if (difference >= unit_value && cid_value > 0) {
-                let money = 0;
-                while (difference >= unit_value && cid_value > 0) {
-                    difference = fixedValue(difference - unit_value);
-                    
-                    money = fixedValue(money + unit_value);
-                    cid_value = fixedValue(cid_value - unit_value);
-                }
-                arr.push([cid_currency, money]);
-            }
+            difference = fixedNumber(difference - getCash);
+            drawerCash = fixedNumber(drawerCash - getCash);
 
-            if (cid_value > 0) moneyLeft = true;
+            change.push(
+                [
+                    currency,
+                    getCash,
+                ]
+            );
+        }
+    }
+    change.reverse();
 
-            return arr;
-        }, []);
-
-    if (difference > 0) {
+    if (drawerCash === 0) {
+        status = statusString[1];
+    }
+    if (drawerCash > 0 && difference > 0) {
         status = statusString[2];
         change = [];
-    }
-    if (difference === 0 && !moneyLeft) {
-        status = statusString[1];
-        change = [...cid];
     }
 
     return {
@@ -58,17 +140,5 @@ function checkCashRegister(price, cash, cid) {
 }
 
 console.log(checkCashRegister(
-    19.5,
-    20,
-    [
-        ["PENNY", 1.01],
-        ["NICKEL", 2.05],
-        ["DIME", 3.1],
-        ["QUARTER", 4.25],
-        ["ONE", 90],
-        ["FIVE", 55],
-        ["TEN", 20],
-        ["TWENTY", 60],
-        ["ONE HUNDRED", 100]
-    ]
+    18.54, 20, [["PENNY", 0.01], ["NICKEL", 0.1], ["DIME", 0.1], ["QUARTER", 0.25], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]
 ));
