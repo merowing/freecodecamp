fd57a807355937462d77eeae776bb5693c2cfdfa
elgynart <elgynart@gmail.com>
2023-02-26 11:01:23 +0200

    fixed the code

--- JavaScript Algorithms and Data Structures/Roman Numeral Converter.js
+++ JavaScript Algorithms and Data Structures/Roman Numeral Converter.js
@@ -15,82 +15,79 @@ const numbers = {
 };
 
 // method 1
-// function convertToRoman(num) {
+function convertToRoman(num) {
 
-//     const keys = Object.keys(numbers);
+    const keys = Object.keys(numbers);
 
-//     function arabicsSequence(firstNumber, pow = 0) {
-//         let arr = [];
-//         const previousIndex = (num) => {
-//             return keys.findIndex(el => el > num) - 1;
-//         }
-//         const dividerIndex = previousIndex(firstNumber);
-//         let divider = (dividerIndex < 0)
-//             ? keys.at(-1)
-//             : keys[dividerIndex];
+    function arabicsSequence(firstNumber, pow = 0) {
+        let arr = [];
+        const previousIndex = (num) => {
+            return keys.findIndex(el => el > num) - 1;
+        }
+        const dividerIndex = previousIndex(firstNumber);
+        let divider = (dividerIndex < 0)
+            ? keys.at(-1)
+            : keys[dividerIndex];
         
-//         while (firstNumber > 0) {
-//             const remainder = firstNumber % divider;
+        while (firstNumber > 0) {
+            const remainder = firstNumber % divider;
             
-//             if (divider > 1 * 10**pow) {
-//                 const num = firstNumber - remainder;
-//                 if (!keys.includes(num)) {
-//                     let returnedArray = arabicsSequence(num, ++pow);
-//                     arr = [...arr, ...returnedArray];
-//                     pow -= 1;
-//                 } else {
-//                     arr.push(num);
-//                 }
+            if (divider > 1 * 10**pow) {
+                const num = firstNumber - remainder;
+                if (!keys.includes(num)) {
+                    let returnedArray = arabicsSequence(num, ++pow);
+                    arr = [...arr, ...returnedArray];
+                    pow -= 1;
+                } else {
+                    arr.push(num);
+                }
 
-//                 divider = keys[ previousIndex(remainder) ];
-//                 firstNumber = remainder;
-//             } else {
-//                 arr.push(divider);
-//                 firstNumber -= divider;
-//             }
-//         }
+                divider = keys[ previousIndex(remainder) ];
+                firstNumber = remainder;
+            } else {
+                arr.push(divider);
+                firstNumber -= divider;
+            }
+        }
 
-//         return arr;
-//     }
+        return arr;
+    }
 
-//     const arabics = arabicsSequence(num);
-//     num = (arabics.length)
-//         ? arabics.map(item => numbers[item]).join('')
-//         : '';
+    const arabics = arabicsSequence(num);
+    num = (arabics.length)
+        ? arabics.map(item => numbers[item]).join('')
+        : '';
 
-//     return num;
-// }
-
-// console.log(convertToRoman(45));
+    return num;
+}
 
 // method 2
-// function convertToRoman(num) {
-//     const sequence = num.toString().split('');
-//     let pow = sequence.length - 1;
-//     num = sequence.reduce((arr, strNumber) => {
-//         const number = parseInt(strNumber);
-//         let int = number * 10**pow;
-//         let temp_arr = [];
+function convertToRoman(num) {
+    const sequence = num.toString().split('');
+    let pow = sequence.length - 1;
+    num = sequence.reduce((arr, strNumber) => {
+        const number = parseInt(strNumber);
+        let int = number * 10 ** pow;
+        let temp_arr = [];
 
-//         while(!numbers[int] && int > 0) {
-//             int = int - 10**pow;
-//             temp_arr.push(10**pow);
-//         }
+        while (!numbers[int] && int > 0) {
+            int = int - 10 ** pow;
+            temp_arr.push(10 ** pow);
+        }
 
-//         if(int > 0) {
-//             arr = [...arr, int, ...temp_arr];
-//         }
+        if (int > 0) {
+            arr = [...arr, int, ...temp_arr];
+        }
 
-//         pow -= 1;
-//         return arr;
-//     }, []);
+        pow -= 1;
+        return arr;
+    }, []);
 
-//     num = num.map(elem => numbers[elem]).join('');
+    num = num.map(elem => numbers[elem])
+        .join('');
 
-//     return num;
-// }
-
-// console.log(convertToRoman(3999));
+    return num;
+}
 
 // method 3
 function convertToRoman(num) {
@@ -125,5 +122,3 @@ function convertToRoman(num) {
 
     return num;
 }
-
-console.log(convertToRoman(45));
