b4672137069723b3d94417bd6bef7066e564ae7a
elgynart <elgynart@gmail.com>
2023-02-26 20:54:13 +0200

    method 2

--- JavaScript Algorithms and Data Structures/Caesars Cipher.js
+++ JavaScript Algorithms and Data Structures/Caesars Cipher.js
@@ -1,27 +1,41 @@
-let rot13 = (str) => {
-    const code = 'abcdefghijklmnopqrstuvwxyz';
-    let decode = '';
+// method 1
+// function rot13(str) {
+//     const code = 'abcdefghijklmnopqrstuvwxyz';
+//     let decode = '';
 
-    str = str.toLowerCase();
-    for (let char of str) {
-        let ind = code.indexOf(char);
+//     str = str.toLowerCase();
+//     for (let char of str) {
+//         let ind = code.indexOf(char);
 
-        if (ind >= 0) {
-            if (ind >= 13) {
-                ind -= 13
-            } else {
-                ind += 13;
-            }
-        }
+//         if (ind >= 0) {
+//             if (ind >= 13) {
+//                 ind -= 13
+//             } else {
+//                 ind += 13;
+//             }
+//         }
         
-        decode += (ind >= 0)
-            ? code[ind]
-            : char;
+//         decode += (ind >= 0)
+//             ? code[ind]
+//             : char;
 
-        decode = decode.toUpperCase();
-    }
+//         decode = decode.toUpperCase();
+//     }
     
-    return decode;
+//     return decode;
+// }
+
+// method 2
+function rot13(str) {
+    let letters = 'abcdefghijklmnopqrstuvwxyzabcdefghijklm';
+
+    str = str.toLowerCase().replace(/[a-z]/g, (p) => {
+        let ind = letters.indexOf(p) + 13;
+        return letters[ind];
+    });
+    str = str.toUpperCase();
+
+    return str;
 }
 
-console.log(rot13('serr cvmmn!'));
+console.log(rot13('free pizza!'));
